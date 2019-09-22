c38c828198901f86ec48058bf280bacb3f275ab2
chenzf <372624240@qq.com>
2019-09-22 12:36:56 +0800

    格式化

--- 彩虹.py
+++ 彩虹.py
@@ -1,10 +1,11 @@
 import turtle
-q = turtle.Pen()
+
+pen = turtle.Pen()
 turtle.bgolor('black')
 sides = 7
 colors = ['red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'purple']
 for x in range(360):
-		q.pencolor(colors[x%sides])
-		q.forward(x*3/sides+x)
-		q.left(360/sides+1)
-		q.width(x*sides/200)
+    pen.pencolor(colors[x % sides])
+    pen.forward(x * 3 / sides + x)
+    pen.left(360 / sides + 1)
+    pen.width(x * sides / 200)
